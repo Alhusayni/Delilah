7964de5759b40761410571014d8eaecc0c1cf11e
alhusayni <alhusayni.aa@gmail.com>
2020-01-30 22:34:05 -0500

    FrontEnd stuff

--- dililah/settings.py
+++ dililah/settings.py
@@ -113,7 +113,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'maindililah/static')
 
 LOGIN_REDIRECT_URL = '/'
 
@@ -121,6 +124,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-
